d503f46d7ef6a1918f6719feb9a904a1f66b62cb
xxzytu <Riddhiman.reddy@gmail.com>
2025-05-16 10:31:32 +0530

    very basic nav bar w/text

--- reclaim/app/page.tsx
+++ reclaim/app/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function home() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-gradient-to-r from-purple-600 to-blue-500 text-#fff">
-        <div className="container align-center py-2 border-2px-gray-solid border-rounded-10">
-          <nav className="flex items-center justify-between py-4 bg-white shadow rounded-lg border-2px-gray-solid border-rounded-10 ">
-            <div className="text-4xl font-bold flex items-center justify-between m-10">Reclaim</div>
+      <header className="bg-gradient-to-r from-purple-600 to-blue-500 ">
+        <div className="container mx-auto px-6 py-4">
+          <nav className="flex items-center justify-between py-4 shadow rounded-lg px-6 bg-white ">
+            <div className="text-4xl font-bold flex items-center justify-between m-10 text-black">Reclaim</div>
           </nav>
         </div>
       </header>
