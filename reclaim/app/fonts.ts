2837e100cc510bf3f2f7c13dcacbadf6324772f5
xxzytu <Riddhiman.reddy@gmail.com>
2025-05-16 11:45:55 +0530

    added font.ts for custom fonts

---
+++ reclaim/app/fonts.ts
@@ -0,0 +1,18 @@
+import { Inter, Roboto_Mono, Outfit} from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+
+})
+
+export const roboto_Mono = Roboto_Mono({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const outfit = Outfit({
+    subsets: ['latin'],
+    display: swap,
+    
+})
--- reclaim/app/page.tsx
+++ reclaim/app/page.tsx
@@ -5,7 +5,7 @@ export default function home() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-gradient-to-r from-purple-600 to-blue-500 ">
-        <div className="container mx-auto px-6 py-4">
+        <div className="container mx-auto px-2 py-2">
           <nav className="flex items-center justify-between py-4 shadow rounded-lg px-6 bg-white ">
             <div className="text-4xl font-bold flex items-center justify-between m-10 text-black">Reclaim</div>
           </nav>
