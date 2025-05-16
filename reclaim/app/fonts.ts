6b7eff936eefa3d7258b3c9d71a93796947ffd09
xxzytu <Riddhiman.reddy@gmail.com>
2025-05-16 12:19:00 +0530

    bug-fixing fonts, need to add tailwind manually

--- reclaim/app/fonts.ts
+++ reclaim/app/fonts.ts
@@ -1,18 +1,21 @@
 import { Inter, Roboto_Mono, Outfit} from 'next/font/google';
 
 export const inter = Inter({
+    variable: '--primary-font',
     subsets: ['latin'],
     display: 'swap',
 
 })
 
 export const roboto_Mono = Roboto_Mono({
+    variable: '--secondary-font',
     subsets: ['latin'],
     display: 'swap',
 })
 
 export const outfit = Outfit({
+    variable: '--main-font',
     subsets: ['latin'],
-    display: swap,
+    display: 'swap',
     
 })
